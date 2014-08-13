cc3cfa2767ba56f27cabfe8327914f85ec4cf38b
Tilo Mitra <tilomitra@gmail.com>
2014-08-13 10:48:48 -0400

    replace config.json with a config folder

---
+++ config/index.js
@@ -0,0 +1,13 @@
+//Store your API Keys and stuff here.
+'use strict';
+
+var path = require('path');
+module.exports = {
+    dirs: {
+        pub     : path.resolve('public/'),
+        bower   : path.resolve('bower_components/'),
+        views   : path.resolve('views/'),
+        layouts : path.resolve('views/layouts/'),
+        partials: path.resolve('views/partials/')
+    }
+};
